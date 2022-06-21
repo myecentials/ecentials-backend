8261e74f609daba9e3b84c99fd90650a5514cb7c
Jesse <iamjesse75@gmail.com>
2022-06-21 09:22:26 +0000

    redirected database to mongodb atlas online database

--- private/database/mongodb.js
+++ private/database/mongodb.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 module.exports = (db_name) => {
-    return mongoose.connect(`mongodb://127.0.0.1/${db_name}`, () => {
+    // return mongoose.connect(`mongodb://127.0.0.1/${db_name}`, () => {
+    //     console.log("MongoDB Connection Successful")
+    // }, e => console.error(e))
+
+    return mongoose.connect(`mongodb+srv://ecentials:0Ef1RfIbYghJoKJK@cluster0.k0eejno.mongodb.net/?retryWrites=true&w=majority`, () => {
         console.log("MongoDB Connection Successful")
     }, e => console.error(e))
+
 }
