aa1d8c54f973fb3222aa34863aea5b4eb18c30a3
lateofrederick <lateofrederick@gmail.com>
2022-06-28 22:07:36 +0000

    set url paths

--- routes/v1/routes.js
+++ routes/v1/routes.js
@@ -5,5 +5,8 @@ router.use('/api/v1/user', require('./User/auth'))
 router.use('/api/v1/user/account', require('./User/userAccount'))
 router.use('/api/v1/user/logs', require('./User/logData'))
 router.use('/api/v1/user/notifications', require('./User/Notifications/notification'))
+router.use('/api/v1/user/favourites', require('./User/favourites'))
+router.use('/api/v1/user/orders', require('./User/orders'))
+router.use('/api/v1/user/cart', require('./User/cart'))
 
 module.exports = router
