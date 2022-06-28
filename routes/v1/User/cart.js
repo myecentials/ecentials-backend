9f84824f65741f79242fd476db37ca9cde03b04a
lateofrederick <lateofrederick@gmail.com>
2022-06-28 22:07:09 +0000

    delete a cart item

--- routes/v1/User/cart.js
+++ routes/v1/User/cart.js
@@ -19,5 +19,27 @@ router.post('/add-item', verify, async (req, res) => {
             }
             return res.status(200).json({message: 'success', data: result});
         });
+    } else {
+        return res.status(400).json({message: 'Please provide the needed data'});
     }
 });
+
+
+// endpoint for authorized user to remove an item from cart
+router.delete('/remove-item-from-cart', verify, async (req, res) => {
+    user_id = req.user._id;
+    const { item_id } = req.body;
+
+    await Cart.deleteOne(
+        { user_id, _id: item_id },
+        (err, _) => {
+            if (err) return res.status(400).json({message: 'Failed to remove favourite'});
+
+            return res.status(200).json({message: 'Favourite removed successfully'})
+        }
+
+    ).clone();
+});
+
+
+module.exports = router;
