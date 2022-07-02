bbf33811a3f16e5044bc0ddd41bf42e651c478fb
lateofrederick <lateofrederick@gmail.com>
2022-07-02 21:53:48 +0000

    set url paths

--- routes/v1/routes.js
+++ routes/v1/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-
+// user related endpoints
 router.use('/api/v1/user', require('./User/auth'))
 router.use('/api/v1/user/account', require('./User/userAccount'))
 router.use('/api/v1/user/logs', require('./User/logData'))
@@ -8,5 +8,11 @@ router.use('/api/v1/user/notifications', require('./User/Notifications/notificat
 router.use('/api/v1/user/favourites', require('./User/favourites'))
 router.use('/api/v1/user/orders', require('./User/orders'))
 router.use('/api/v1/user/cart', require('./User/cart'))
+
+// pharmacy related details endpoints
+router.use('/api/v1/pharmacies', require('./Pharmacy/pharmacy'))
+
+// pescription related endpoints
+router.use('/api/v1/prescription', require('./Prescription/prescription'))
 
 module.exports = router
