2774c4c074245398a515a5a6a861f16813e7e127
lateofrederick <lateofrederick@gmail.com>
2022-07-04 10:09:04 +0000

    fetch specific type of notification

--- routes/v1/User/Notifications/notification.js
+++ routes/v1/User/Notifications/notification.js
@@ -7,8 +7,7 @@ const User = require('../../../../private/schemas/User');
 
 // creates a new notification for a verified user
 router.post('/add_notification_for_user', verify, async (req, res) => {
-    const user_id = req.user._id;
-    const { title, message, image } = req.body;
+    const { user_id, title, message, image, notification_type } = req.body;
 
     const isUserPresent = await User.findOne({
         _id: user_id
@@ -22,7 +21,7 @@ router.post('/add_notification_for_user', verify, async (req, res) => {
 
     if (!!title && !!message && !!image) {
         await Notification.create({
-            title, message, image, user_id
+            title, message, image, user_id, notification_type
         }, ((err, result) => {
             if (err) {
                 return res.status(400).json({ message: "Failed to save notification" })
@@ -39,17 +38,34 @@ router.post('/add_notification_for_user', verify, async (req, res) => {
 
 
 // retrieve all notifications for a verified user
-router.get('/fetch-user-notifications', verify, async (req, res) => {
+router.get('/fetch-all-user-notifications', verify, async (req, res) => {
     const user_id = req.user._id
 
     try{
-        const notifications = await Notification.find({user_id: user_id, status: 0})
+        const notifications = await Notification.find({ user_id: user_id })
 
-        if(notifications) return res.status(200).json({ message: notifications })
+        if(notifications) return res.status(200).json({ message: "success", data: notifications })
     }
     catch(e){
         res.status(400).json({ message: e })
     }
 });
+
+// this route allows a user to fetch a specific notification or recommended
+// notification for the user
+router.get('/fetch-user-notifications', verify, async (req, res) => {
+    const user_id = req.user._id;
+    const { notification_type } = req.body;
+
+    try{
+        const notifications = await Notification.find({ user_id, notification_type })
+
+        if(notifications) return res.status(200).json({ message: "success", data: notifications })
+        else return res.status(400).json({ message: 'Failed to retrieve notifications'})
+    }
+    catch(e){
+        res.status(400).json({ message: e })
+    }
+})
 
 module.exports = router;
