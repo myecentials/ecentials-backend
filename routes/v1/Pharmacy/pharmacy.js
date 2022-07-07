2cd18c7d3c8c856e0679c21fa87f5aacbd118984
lateofrederick <lateofrederick@gmail.com>
2022-07-07 19:15:29 +0000

    create a new pharmacy

--- routes/v1/Pharmacy/pharmacy.js
+++ routes/v1/Pharmacy/pharmacy.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { encryptPassword } = require('../../../private/helpers/functions');
 const Store = require('../../../private/schemas/Store');
 const verify = require('../../../verifyToken')
 
@@ -13,6 +14,40 @@ router.get('', verify, async (req, res) => {
         res.status(200).json({message: 'success', data: result})
     }).clone();
 });
+
+
+// create a new pharmacy
+router.post('/create-new-pharmacy', verify, async (req, res) => {
+    const user_id = req.user._id;
+
+    const { 
+        store_pin, 
+        name, 
+        description, 
+        address, 
+        city, 
+        licence_no,
+        logo,
+        accept_nhis,
+        gps_lat,
+        gps_lng,
+     } = req.body;
+
+     if (!!name && !!licence_no && !!city) {
+         await Store.create({
+             user_id, name, description, address, city, licence_no, logo, accept_nhis,
+             gps_lat, gps_lng, store_pin: encryptPassword(store_pin)
+         }, (err, result) => {
+             if (err) {
+                 return res.status(400).json({ message: 'Failed to create pharmacy.' });
+             }
+             return res.status(200).json({ message: 'success', data: result});
+         });
+     } else {
+         return res.status(403).json({ message: 'Please make sure the correct data is provided' });
+     }
+});
+
 
 module.exports = router;
     
