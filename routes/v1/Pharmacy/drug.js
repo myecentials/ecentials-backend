fa9ba86ac56998e0df6dbfc06443e1aee8bc8ce3
lateofrederick <lateofrederick@gmail.com>
2022-07-12 15:39:01 +0000

    create a new drug

---
+++ routes/v1/Pharmacy/drug.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+const Drug = require('../../../private/schemas/Drug');
+const verify = require('../../../verifyToken')
+
+
+// allows a verified pharmacy to add a new drug to their 
+// drugs catalog.
+router.post('/add-new-drug', verify, async (req, res) => {
+    const {
+        store_id,
+        name,
+        prize,
+        description,
+        dosage,
+        quantity,
+        dosage_form,
+        manufacturer,
+        views,
+        discount,
+        nhis,
+        images
+    } = req.body;
+
+    if (!!name && !!manufacturer && !!description) {
+        Drug.create({
+            store_id, name, prize, description, dosage, quantity, dosage_form, manufacturer, views,
+            discount, nhis, images
+        }, (err, result) => {
+            if (err) {
+                return res.status(400).json({ message: "Failed to add drug. Try again later." });
+            }
+            return res.status(200).json({ message: 'success', data: result })
+        });
+    } else {
+        res.status(403).json({ message: 'Please make sure you have provided the needed details.'});
+    }
+});
+
+module.exports = router;
